774958cd1cd4bda6fe1760df95f8ab0fa05b80b0
Hoa <me@hoa.do>
2023-07-26 10:33:14 +1000

    revert to fps for imageio

--- animatediff/utils/util.py
+++ animatediff/utils/util.py
@@ -24,9 +24,8 @@ def save_videos_grid(videos: torch.Tensor, path: str, rescale=False, n_rows=6, f
         x = (x * 255).numpy().astype(np.uint8)
         outputs.append(x)
 
-    duration = 1000 / fps
     os.makedirs(os.path.dirname(path), exist_ok=True)
-    imageio.mimsave(path, outputs, duration=duration)
+    imageio.mimsave(path, outputs, fps=fps)
 
 
 # DDIM Inversion
