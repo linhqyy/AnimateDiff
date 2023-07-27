f37bad427d6f66c136d95da949a01a723b1a8204
Hoa <me@hoa.do>
2023-07-27 18:10:49 +1000

    Say a prayer

--- animatediff/utils/convert_lora_with_backup.py
+++ animatediff/utils/convert_lora_with_backup.py
@@ -4,18 +4,30 @@ import torch
 import os
 
 loaded_loras = None
-original_unet = {}
-original_text_encoder = {}
+original_weight = {}
 LORA_PREFIX_UNET = "lora_unet"
 LORA_PREFIX_TEXT_ENCODER = "lora_te"
+network_updates = defaultdict(dict)
 
 def load_loras(pipeline, loras, device):
-    global loaded_loras
+    global loaded_loras, network_updates
     if loras == loaded_loras:
         print("No Loras changed")
     else:
-        print("Loras changed")
-        restore_networks(pipeline)
+        if loaded_loras is not None:
+            restore_networks(pipeline, network_updates)
+
+        # Reset network updates for new Lora weights
+        network_updates = defaultdict(dict)
+        for lora in loras:
+            lora_path = lora['path']
+            # Check if path exists
+            if lora_path == "none" or not os.path.exists(lora_path):
+                continue
+            state_dict = load_file(lora_path, device=device)
+            # Getting all network updates
+            network_updates = get_network_updates(network_updates, state_dict=state_dict)
+            backup_networks(pipeline, network_updates)
         for lora in loras:
             lora_path = lora['path']
             # Check if path exists
@@ -29,60 +41,58 @@ def load_loras(pipeline, loras, device):
             loaded_loras = loras
     return pipeline
 
-def restore_networks(pipeline):
-    global original_unet, original_text_encoder
-    for temp_name in original_unet:
-        curr_layer = pipeline.unet.__getattr__(temp_name)
-        curr_layer.weight.data = original_unet[temp_name].clone().detach()
-    for temp_name in original_text_encoder:
-        curr_layer = pipeline.text_encoder.__getattr__(temp_name)
-        curr_layer.weight.data = original_text_encoder[temp_name].clone().detach()
 
-
-def backup_networks(pipeline, state_dict):
-    global original_unet, original_text_encoder
-    updates = defaultdict(dict)
+def get_network_updates(network_updates, state_dict):
+    
     for key, value in state_dict.items():
-        # it is suggested to print out the key, it usually will be something like below
-        # "lora_te_text_model_encoder_layers_0_self_attn_k_proj.lora_down.weight"
+    # it is suggested to print out the key, it usually will be something like below
+    # "lora_te_text_model_encoder_layers_0_self_attn_k_proj.lora_down.weight"
 
         layer, elem = key.split('.', 1)
-        updates[layer][elem] = value
+        network_updates[layer][elem] = value
 
-    for layer, elems in updates.items():
-        print("layer")
-        print(layer)
+    return network_updates
 
-        if "text" in layer:
-            layer_infos = layer.split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split("_")
-            curr_layer = pipeline.text_encoder
-        else:
-            layer_infos = layer.split(LORA_PREFIX_UNET + "_")[-1].split("_")
-            curr_layer = pipeline.unet
+def get_target_layer(layer, pipeline):
+    if "text" in layer:
+        layer_infos = layer.split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split("_")
+        curr_layer = pipeline.text_encoder
+    else:
+        layer_infos = layer.split(LORA_PREFIX_UNET + "_")[-1].split("_")
+        curr_layer = pipeline.unet
 
-        # find the target layer
-        temp_name = layer_infos.pop(0)
-        print("temp_name")
-        print(temp_name)
-        while len(layer_infos) > -1:
-            try:
-                curr_layer = curr_layer.__getattr__(temp_name)
-                if len(layer_infos) > 0:
-                    temp_name = layer_infos.pop(0)
-                elif len(layer_infos) == 0:
-                    break
-            except Exception:
-                if len(temp_name) > 0:
-                    temp_name += "_" + layer_infos.pop(0)
-                else:
-                    temp_name = layer_infos.pop(0)
-        print(temp_name)
-        if "text" in layer:
-            if temp_name not in original_unet:
-                original_unet[temp_name] = curr_layer.weight.data.clone().detach()
-        else:
-            if temp_name not in original_text_encoder:
-                original_text_encoder[temp_name] = curr_layer.weight.data.clone().detach()
+    # find the target layer
+    temp_name = layer_infos.pop(0)
+    while len(layer_infos) > -1:
+        try:
+            curr_layer = curr_layer.__getattr__(temp_name)
+            if len(layer_infos) > 0:
+                temp_name = layer_infos.pop(0)
+            elif len(layer_infos) == 0:
+                break
+        except Exception:
+            if len(temp_name) > 0:
+                temp_name += "_" + layer_infos.pop(0)
+            else:
+                temp_name = layer_infos.pop(0)
+    return curr_layer
+
+
+def backup_networks(pipeline, network_updates):
+    global original_weight
+    for layer, elems in network_updates.items():
+        curr_layer = get_target_layer(layer, pipeline)
+        original_weight[layer] = curr_layer.weight.data.clone().detach()
+    return pipeline
+
+def restore_networks(pipeline, network_updates):
+    global original_weight
+    for layer, elems in network_updates.items():
+        curr_layer = get_target_layer(layer, pipeline)
+        # if layer in original_text_encoder:
+        curr_layer.weight.data = original_weight[layer].clone().detach()
+
+    return pipeline
 
 
 # Modified code from https://github.com/huggingface/diffusers/issues/3064#issuecomment-1514082155
@@ -100,43 +110,11 @@ def load_lora_weights(pipeline, state_dict, multiplier, device, dtype):
     pipeline.to(device)
 
     # load LoRA weight from .safetensors
+    network_updates = defaultdict(dict)
+    network_updates = get_network_updates(network_updates=network_updates, state_dict=state_dict)
 
-    updates = defaultdict(dict)
-    for key, value in state_dict.items():
-        # it is suggested to print out the key, it usually will be something like below
-        # "lora_te_text_model_encoder_layers_0_self_attn_k_proj.lora_down.weight"
-
-        layer, elem = key.split('.', 1)
-        updates[layer][elem] = value
-
-    index = 0
-    # directly update weight in diffusers model
-    for layer, elems in updates.items():
-        index += 1
-        print(layer)
-
-        if "text" in layer:
-            layer_infos = layer.split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split("_")
-            curr_layer = pipeline.text_encoder
-        else:
-            layer_infos = layer.split(LORA_PREFIX_UNET + "_")[-1].split("_")
-            curr_layer = pipeline.unet
-
-        # find the target layer
-        temp_name = layer_infos.pop(0)
-        while len(layer_infos) > -1:
-            print(temp_name)
-            try:
-                curr_layer = curr_layer.__getattr__(temp_name)
-                if len(layer_infos) > 0:
-                    temp_name = layer_infos.pop(0)
-                elif len(layer_infos) == 0:
-                    break
-            except Exception:
-                if len(temp_name) > 0:
-                    temp_name += "_" + layer_infos.pop(0)
-                else:
-                    temp_name = layer_infos.pop(0)
+    for layer, elems in network_updates.items():
+        curr_layer = get_target_layer(layer, pipeline)
 
         # get elements for this layer
         weight_up = elems['lora_up.weight'].to(dtype)
@@ -147,13 +125,6 @@ def load_lora_weights(pipeline, state_dict, multiplier, device, dtype):
         else:
             alpha = 1.0
         
-        # if (backup):
-        #     if original_weights[temp_name] is None:
-        #         original_weights[index] = curr_layer.weight.data.clone().detach()
-        # else:
-        #     curr_layer.weight.data = original_weights[index].clone().detach()
-
-        # update weight
         if len(weight_up.shape) == 4:
             curr_layer.weight.data += multiplier * alpha * torch.mm(weight_up.squeeze(3).squeeze(2), weight_down.squeeze(3).squeeze(2)).unsqueeze(2).unsqueeze(3)
         else:
